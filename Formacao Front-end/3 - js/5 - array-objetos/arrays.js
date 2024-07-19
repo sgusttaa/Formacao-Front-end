3116ac94de9193a477f56bfb90899c8a1b8b9aee
gustavo <gustavosilvap2005@gmail.com>
2024-07-18 22:04:06 -0300

    att array-objetos

--- Formacao Front-end/3 - js/5 - array-objetos/arrays.js
+++ Formacao Front-end/3 - js/5 - array-objetos/arrays.js
@@ -163,3 +163,91 @@ letters.unshift('p', 'q', 'r')//adiciona novos elementos no comeco da array
 letters.unshift('z')
 
 console.log(letters)
+
+// 13 - indexOf e lastIndexOf
+
+const myElements = ['morango', 'maça', 'abacate', 'pera', 'banana', 'maça']
+
+console.log(myElements.indexOf('abacate')); //indica a posicao do elemento pesquisado
+console.log(myElements.lastIndexOf('maça')); //indica a posicao do ultimo elemento pesquisado
+
+console.log(myElements.indexOf('mamao'))
+console.log(myElements.lastIndexOf('mamao'))
+
+// 14 - slice  define um intervalo para espeficifar o fim e o inicio de uma sequencia
+
+const testeSlice = ['a', 'b', 'c', 'd', 'e', 'f']
+
+const subArray = testeSlice.slice(2, 4)
+console.log(subArray)
+console.log(testeSlice)
+
+const subArray2 = testeSlice.slice(2, 4 + 1)
+console.log(subArray2)
+
+const subArray3 = testeSlice.slice(10, 20)
+console.log(subArray3)
+
+const subArray4 = testeSlice.slice(2) // a partir do elemento 2
+console.log(subArray4)
+
+// 15 - forEach (para cada)
+
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((numero) => {
+    console.log(`o número é ${numero}`)
+})
+
+const posts = [
+    {title: 'Primeiro post', category:'PHP'},
+    {title: 'Segundo post', category:'JavaScript'},
+    {title: 'Terceiro post', category:'Python'}
+]
+
+posts.forEach((post) => {
+    console.log(`Exibindo: ${post.title}, da categoria ${post.category}`)
+})
+
+// 16 - includes retorna um valor booleano(true ou false)
+
+const brands = ['BMW', 'VW', 'Fiat']
+
+console.log(brands.includes('Fiat'))
+console.log(brands.includes('Fat'))
+
+if(brands.includes('BMW')){
+    console.log('Há carros da marca BMW')
+}
+
+// 17 - reverse inverte a lista
+
+const reverseTest = [1, 2, 3, 4, 5]
+
+reverseTest.reverse();
+console.log(reverseTest)
+
+// 18 - trim  nao modifica a string original
+
+const trimTest = '    testando \n  '
+
+console.log(trimTest)
+console.log(trimTest.trim())
+
+console.log(trimTest.length)
+console.log(trimTest.trim().length)
+
+// 19 - padStart
+
+const padStart = '100'
+
+const newNumber = padStart.padStart(4, '0')
+
+console.log(padStart)
+console.log(newNumber)
+
+const padEnd = newNumber.padEnd(10, '0')
+
+console.log(padEnd)
+
+// 20 - split
